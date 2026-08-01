22c50e352eeb3aba7da01f32c622586d8de2b155
Miloš Šilhár <migmig095@gmail.com>
2026-08-01 13:56:22 +0200

    added postgres drop and recreate

--- just/yesno.sh
+++ just/yesno.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-read -p "Do you want to proceed? (yes/NO) " yn
+read -p "Do you want to proceed? [yes/NO] " yn
 
 case $yn in 
 	yes ) exit 0;;
-	* ) echo Will not do it;
-		exit 1;;
+	* ) echo "You must type 'yes' to proceed. Will not do it."; exit 1;;
 esac
